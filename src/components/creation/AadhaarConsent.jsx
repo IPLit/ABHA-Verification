8fd8701b0e1bc3682f75a1109baccbae7a2b0d0e
shilpa-iplit <sparab@iplit.in>
2024-07-31 21:42:29 +0530

    NHA suggestions updated

--- src/components/creation/AadhaarConsent.jsx
+++ src/components/creation/AadhaarConsent.jsx
@@ -17,11 +17,11 @@ const AadhaarConsent = (props) => {
         " I understand that my ABHA number can be used and shared for purposes as may be notified by ABDM from time to time including provision of healthcare services. Further, I am aware that my personal identifiable information (Name, Address, Age, Date of Birth, Gender and Photograph) may be made available to the entities working in the National Digital Health Ecosystem (NDHE) which inter alia includes stakeholders and entities such as healthcare professionals (e.g. doctors), facilities (e.g. hospitals, laboratories) and data fiduciaries (e.g. health programmes), which are registered with or linked to the Ayushman Bharat Digital Mission (ABDM), and various processes there under.\n" +
         " I authorize NHA to use my Aadhaar number for performing Aadhaar based authentication with UIDAI as per the provisions of the Aadhaar (Targeted Delivery of Financial and other Subsidies, Benefits and Services) Act, 2016 for the aforesaid purpose. I understand that UIDAI will share my e-KYC details, or response of \"Yes\" with NHA upon successful authentication. I have been duly informed about the option of using other IDs apart from Aadhaar; however, I consciously choose to use Aadhaar number for the purpose of availing benefits across the NDHE. I am aware that my personal identifiable information excluding Aadhaar number / VID number can be used and shared for purposes as mentioned above. I reserve the right to revoke the given consent at any point of time as per provisions of Aadhaar Act and Regulations.\n",
 
-        "I consent to usage of my ABHA address and ABHA number for linking of my legacy (past) government health records and those which will be generated during this encounter.",
+        "I consent to usage of my ABHA address and ABHA number for linking of my legacy health records and those which will be generated during this encounter.",
 
         "I authorize the sharing of all my health records with healthcare provider(s) for the purpose of providing healthcare services to me during this encounter",
 
-        "I consent to the anonymization and subsequent use of my government health records for public health purposes.",
+        "I consent to the anonymization and subsequent use of my past health records for public health purposes.",
 
         `I (${userName}), confirm that I have duly informed and explained the beneficiary of the\n` +
         `contents of consent for aforementioned purposes.`,
